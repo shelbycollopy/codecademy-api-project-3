e6b23f75f42f317c70f7a25159fdbbb4f0e16621
Shelby Collopy <code@shelbycollopy.com>
2018-03-28 14:34:01 -0700

    "" for consistency

--- server.js
+++ server.js
@@ -264,7 +264,7 @@ const requestHandler = (request, response) => {
     return response.end();
   }
 
-  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Origin', "*");
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   response.setHeader(
       'Access-Control-Allow-Headers', 'X-Requested-With,content-type');
